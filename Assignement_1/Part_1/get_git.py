5d34c47c0475f3ffb336579109634cf4269046a1
kunwar <kunwarrattan@live.com>
2015-01-30 20:18:57 -0500

    Part 1

--- Assignement_1/Part_1/get_git.py
+++ Assignement_1/Part_1/get_git.py
@@ -5,17 +5,32 @@ import requests
 from bs4 import BeautifulSoup
 
 FILE = open("Config.txt","r")
-STR1 = FILE.readline()
-VAR = STR1.partition("url = ")
-URL= VAR[2]
-STR2 = FILE.readline();
-print(STR2)
+def start():
+    STR1 = FILE.readline().partition("url = ")
+    URL= STR1[2]
 
-re =requests.get(URL);
+    STR2 = FILE.readline().partition("root_directory=")
+    ROOT = STR2[2]
 
-data = re.text
+    re =requests.get(URL)
+    data = re.text
+    soup = BeautifulSoup(data)
 
-soup = BeautifulSoup(data)
+    for link in soup.find_all('a'):
+        if link.has_attr('href'):
+            var = link.get('href')
+            if (("http" or "https") and "git" ) in var:
+                url = link.get('href')
+                clone(url, ROOT)
+    return
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
+def clone(URL, ROOT_DIRECTORY):
+    import os, sys
+    if not os.path.exists(ROOT_DIRECTORY):
+        os.makedirs(ROOT_DIRECTORY, 0755)
+        cmd_string = "git clone %s %s" %(URL, ROOT_DIRECTORY)
+        os.system(cmd_string)
+    return
+
+
+start()
