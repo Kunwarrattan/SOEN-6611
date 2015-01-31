462f5eb911a73f2ed823ade312327c88c7d5a454
Kunwar Rattan <kunwarrattan@live.com>
2015-01-30 19:25:23 -0500

    Uploading Directory Structure

--- Assignement_1/Part_1/get_git.py
+++ Assignement_1/Part_1/get_git.py
@@ -4,3 +4,18 @@ __author__ = 'kunwar'
 import requests
 from bs4 import BeautifulSoup
 
+FILE = open("Config.txt","r")
+STR1 = FILE.readline()
+VAR = STR1.partition("url = ")
+URL= VAR[2]
+STR2 = FILE.readline();
+print(STR2)
+
+re =requests.get(URL);
+
+data = re.text
+
+soup = BeautifulSoup(data)
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
