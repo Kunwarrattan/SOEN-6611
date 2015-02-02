188e0930fe74382cf27a29d5e8bbdee7cd6ddb9c
Kunwar Rattan <kunwarrattan@live.com>
2015-02-01 22:09:16 -0500

    final commit

--- Assignement_1/Part_2/get_atlassian_bugs.py
+++ Assignement_1/Part_2/get_atlassian_bugs.py
@@ -1,15 +1,15 @@
 __author__ = 'kunwar'
 
-import urllib2
+import urllib2,time
 
 FILE = open("Config.txt","r")
 
 def Filter_File():
     STR1 = FILE.readline().partition("url = ")
-    URL= STR1[2]
+    URL= STR1[2].strip()
 
     STR2 = FILE.readline().partition("project_tag = ")
-    PROJECT_TAG = STR2[2]
+    PROJECT_TAG = STR2[2].strip()
 
     STR3 = FILE.readline().partition("bug_start = ")
     BUG_START = STR3[2]
@@ -23,16 +23,23 @@ def Filter_File():
     STR6 = FILE.readline().partition("root_directory=")
     ROOT_DIRECTORY = STR6[2]
 
-   # from random import randint
-   # for x in range(0, 2):
-    #    MTS = randint(1,MTS)
-    Get_Content(URL,BUG_START,ROOT_DIRECTORY)
+    BUG_NUMBER = int(BUG_START)
+
+    from random import randint
+    for x in range(int(BUG_START), int(BUG_END)):
+        MTS = randint(1,MTS)
+        time.sleep(MTS)
+        string_url = "https://"+URL+".atlassian.net"+"/browse/WEBSITE-"+str(BUG_NUMBER)+"?jql="
+        print string_url
+        Get_Content(string_url,BUG_NUMBER,ROOT_DIRECTORY)
+        BUG_NUMBER = BUG_NUMBER+1
     return
 
 def Get_Content(URL,BUG_Number,ROOT_DIRECTORY):
+
     content = urllib2.urlopen(URL).read()
-    #print content
-    path = ROOT_DIRECTORY +"/"+ BUG_Number
+
+    path = ROOT_DIRECTORY +"/"+ str(BUG_Number)
 
     import os
     if not os.path.exists(path):
@@ -41,7 +48,7 @@ def Get_Content(URL,BUG_Number,ROOT_DIRECTORY):
     else:
         print "Path exists"
 
-    f = open(path+"/"+BUG_Number+".txt",'w+')
+    f = open(path+"/"+str(BUG_Number)+".txt",'w+')
     f.write(content)
     f.read()
     f.close()
